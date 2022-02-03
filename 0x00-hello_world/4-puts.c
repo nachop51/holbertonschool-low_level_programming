68b0c0c38b05a7c773de71b410f4978ebb59f5cd
Nachop51 <nachoperalta0@gmail.com>
2022-02-03 08:31:15 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase
+ *
+ * Return: returns no error
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
