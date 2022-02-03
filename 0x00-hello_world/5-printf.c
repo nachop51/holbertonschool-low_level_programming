5ffb801e15caec7ffcf4372304207ef127419fc9
Nachop51 <nachoperalta0@gmail.com>
2022-02-03 08:35:25 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints out a phrase
+ *
+ * Return: no error
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
