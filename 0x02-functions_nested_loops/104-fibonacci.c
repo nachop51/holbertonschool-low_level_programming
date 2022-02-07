b13e6a6411d42912b229472c358c9362cb0e6a0e
Nachop51 <nachoperalta0@gmail.com>
2022-02-07 23:33:25 +0000

    test

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,13 +10,13 @@ int main(void)
 	unsigned long num1 = 0, num2 = 1, result;
 	int count;
 
-	for (count = 0; count < 96; count++)
+	for (count = 0; count < 97; count++)
 	{
 		result = num1 + num2;
 		num1 = num2, num2 = result;
 
 		printf("%lu", result);
-		if (count != 95)
+		if (count != 96)
 			putchar(44), putchar(32);
 	}
 	putchar(10);
