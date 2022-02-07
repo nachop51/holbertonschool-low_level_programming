0a7f598d5371d1f9531dc4557f08efaff795e08c
Nachop51 <nachoperalta0@gmail.com>
2022-02-07 22:06:44 +0000

    Test

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,9 +6,7 @@
  */
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
-		;
-	else
+	if (n > 0 && n < 16)
 	{
 		int x;
 		int y;
@@ -19,32 +17,34 @@ void print_times_table(int n)
 			for (y = 0; y <= n; y++)
 			{
 				result = x * y;
-				if (result > 9)
+				if (result < 10)
 				{
+					if (y != 0)
+					{
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar(result + '0');
+				}
+				else if (result > 9 && result < 100)
+				{
+					_putchar(' ');
 					_putchar(result / 10 + '0');
 					_putchar(result % 10 + '0');
 				}
-				else if (result > 100)
-				{
-					_putchar(result / 100 + '0');
-					_putchar((result / 10) % 10);
-					_putchar(result % 10);
-				}
 				else
 				{
-				if (y != 0)
-				{
-					_putchar(' ');
+					_putchar(result / 100 + '0');
+					_putchar((result / 10) % 10 + '0');
+					_putchar(result % 10 + '0');
 				}
-					_putchar(result + '0');
-				}
-				if (y != 9)
+				if (y != n)
 				{
 					_putchar(',');
 					_putchar(' ');
 				}
 			}
-		_putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
