d4287d91dcf87e164fe47841f4a70e1d90582620
Nachop51 <nachoperalta0@gmail.com>
2022-02-07 14:59:19 +0000

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,33 +1,23 @@
 #include "main.h"
 
 /**
- * main - prints out if a number greater or lower than 0
+ * print_sign - prints out if a number greater or lower than 0
+ * @number: to check
  *
  * Return: no error
  */
-int main(void)
+int print_sign(int number)
 {
-	int r;
-
-	r = print_sign(98);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0xff);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(-1);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+	if (number > 0)
+	{
+		return (1);
+	}
+	else if (number == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
 }
