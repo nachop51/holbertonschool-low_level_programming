c10331108296fa9a750ed6693e398feb3fcb0caf
Nachop51 <nachoperalta0@gmail.com>
2022-02-07 15:00:50 +0000

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,14 +10,17 @@ int print_sign(int number)
 {
 	if (number > 0)
 	{
+		_putchar('+')
 		return (1);
 	}
 	else if (number == 0)
 	{
+		_putchar('0')
 		return (0);
 	}
 	else
 	{
+		_putchar('-')
 		return (-1);
 	}
 }
