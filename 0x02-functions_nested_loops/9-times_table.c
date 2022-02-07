0337460f0a00ff3ab4d86a7af78f19d59cc81d73
Nachop51 <nachoperalta0@gmail.com>
2022-02-07 16:10:05 +0000

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,8 +16,20 @@ void times_table(void)
 		for (y = 0; y <= 9; y++)
 		{
 			result = x * y;
-			_putchar(result + '0');
-			_putchar(',');
+			if (result > 9)
+			{
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+			if (y != 9)
+			{
+				_putchar(',');
+			}
 			_putchar(' ');
 		}
 		_putchar('\n');
