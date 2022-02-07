baa090ed14423bbe9b0beeef831ab0b6b6918c81
Nachop51 <nachoperalta0@gmail.com>
2022-02-07 15:16:49 +0000

    Test

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,6 +10,11 @@ int print_last_digit(int number)
 {
 	int last_digit = number % 10;
 
+	if (last_digit < 0)
+	{
+		last_digit *= (-1);
+	}
+
 	_putchar(last_digit + '0');
 	return (last_digit);
 }
