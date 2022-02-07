ac354dbe63c4f06b150154f9932a651cfe94aae5
Nachop51 <nachoperalta0@gmail.com>
2022-02-07 14:51:05 +0000

    Test

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * main - checks if is lower or not
+ * _islower - checks if is lower or not
  *
  * Return: no error
  */
-int main(void)
+int _islower(int letter)
 {
-	int r;
-
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+	if (letter > 96 && letter < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
