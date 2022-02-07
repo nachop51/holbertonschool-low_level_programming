882ffcf8a9414f22d561e893bdcffb7e2117a4d0
Nachop51 <nachoperalta0@gmail.com>
2022-02-07 23:38:29 +0000

    test

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,12 +8,12 @@
 int main(void)
 {
 	unsigned long x, y, z, a, b, c, d, e, f;
-	int i;
+	int n;
 
 	x = 1;
 	y = 2;
 	printf("1, 2, ");
-	for (i = 0; i < 90; i++)
+	for (n = 0; n < 90; n++)
 	{
 		z = x + y;
 		printf("%lu, ", (unsigned long) z);
@@ -24,16 +24,16 @@ int main(void)
 	b = x % 10000000000;
 	c = y / 10000000000;
 	d = y % 10000000000;
-	for (i = 90; i < 96; i++)
+	for (n = 90; n < 96; n++)
 	{
 		e = a + c;
 		f = b + d;
-		if (i == 91 || i == 92 || i == 94 || i == 95)
+		if (n == 91 || n == 92 || n == 94 || n == 95)
 		{
 			f = f - 10000000000;
 			e = e + 1;
 		}
-		if (i != 95)
+		if (n != 95)
 			printf("%lu%lu, ", (unsigned long) e, (unsigned long) f);
 		else
 			printf("%lu%lu\n", (unsigned long) e, (unsigned long) f);
