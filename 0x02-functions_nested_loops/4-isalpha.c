c5d3aa1782686ca9c7cd6d0cd00a912f862ead1a
Nachop51 <nachoperalta0@gmail.com>
2022-02-07 14:54:15 +0000

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,23 +1,19 @@
 #include "main.h"
 
 /**
- * main - prints out if a letter is alpha or not
+ * _isalpha - prints out if a letter is alpha or not
+ * @letter: letter to check
  *
  * Return: no error
  */
-int main(void)
+int _isalpha(int letter)
 {
-	int r;
-
-	r = _isalpha('H');
-	_putchar(r + '0');
-	r = _isalpha('o');
-	_putchar(r + '0');
-	r = _isalpha(108);
-	_putchar(r + '0');
-	r = _isalpha(';');
-	_putchar(r + '0');
-	_putchar('\n');
-
-	return (0);
+	if (letter > 64 && letter < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
