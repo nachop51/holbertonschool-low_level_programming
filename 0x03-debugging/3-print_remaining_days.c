187e15486be4daecc44b8625758dd8091e61e219
Nachop51 <nachoperalta0@gmail.com>
2022-02-08 21:06:33 +0000

    debug

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,7 +13,7 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 	{
-		if (month > 2)
+		if (month > 2 && day >= 60)
 		{
 			day++;
 		}
@@ -23,7 +23,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 2 && day >= 60)
+		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
