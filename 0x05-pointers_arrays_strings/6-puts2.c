3685dda5b6b82f519b7cee32e1714248f1826573
Nachop51 <nachoperalta0@gmail.com>
2022-02-11 16:13:59 +0000

    Playing with strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @s: string
+ */
+void puts2(char *s)
+{
+	int i, j = strlen(s) - 1;
+
+	for (i = 0; i < j; i = i + 2)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
