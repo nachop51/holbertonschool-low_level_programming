3d35e890139fdcc04f57aa15f685807c4065fd2d
Nachop51 <nachoperalta0@gmail.com>
2022-02-11 11:18:17 +0000

    Testing pointers values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer 1
+ * @b: pointer 2
+ */
+void swap_int(int *a, int *b)
+{
+	int *fp = a;
+	int *sp = b;
+
+	fp = b;
+	sp = a;
+}
