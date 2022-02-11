957c1ca964fb8be446575ee5fad47f61ae204c05
Nachop51 <nachoperalta0@gmail.com>
2022-02-11 22:57:43 +0000

    My own Atoi :)

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _atoi - converts string to an integer
+ * @s: string
+ *
+ * Return: an integer
+ */
+int _atoi(char *s)
+{
+	char c;
+	unsigned int i, j = _strlen(s) - 1;
+	int flag = 0, sign = 1, n = 0;
+
+	for (i = 0; i <= j; i++)
+	{
+		c = s[i];
+		if (c == '-')
+			sign *= -1;
+		if (c >= 48 && c <= 57)
+		{
+			n += c - 48;
+			n *= 10;
+			flag = 1;
+		}
+		if (flag == 1)
+		{
+			if (!(c >= 48 && c <= 57))
+				break;
+		}
+	}
+	if (sign == -1)
+	{
+		n *= -1;
+	}
+	n /= 10;
+	return (n);
+}
+
+/**
+ * _strlen - string length
+ * @s: string
+ *
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	int i;
+	char a;
+
+	for (i = 0; i > -1 ; i++)
+	{
+		a = s[i];
+		if (a == '\0')
+			break;
+	}
+	return (i);
+}
