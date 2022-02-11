597bae0a6d516e1b3c7da40d85728c8e9e436788
Nachop51 <nachoperalta0@gmail.com>
2022-02-11 17:00:40 +0000

    Test

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_array - prints an array
+ * @a: the array
+ * @n: size of the array
+ */
+void print_array(int *a, int n)
+{
+	int i, j;
+
+	for (i = 0; i <= n - 1; i++)
+	{
+		if (i != n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		}
+	}
+}
