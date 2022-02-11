50f324657cdd479dfb645d51e75701336582c058
Nachop51 <nachoperalta0@gmail.com>
2022-02-11 11:39:31 +0000

    Testing strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strlen - length of a string
+ * @s: parameter
+ *
+ * Return: returns the length of a string
+ */
+int _strlen(char *s)
+{
+
+	return (strlen(s));
+}
