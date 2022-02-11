c5e7ff5917f2c546ee8316e64cc6dd07c8e4ca68
Nachop51 <nachoperalta0@gmail.com>
2022-02-11 17:45:56 +0000

    Puts 2 function

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,31 @@
  */
 void puts2(char *s)
 {
-	int i, j = strlen(s) - 1;
+	int i, j = _strlen(s) - 1;
 
 	for (i = 0; i <= j; i = i + 2)
 	{
 		_putchar(s[i]);
 	}
 	_putchar('\n');
+}
+
+/**
+ * _strlen - string length
+ * @s: string
+ *
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	int i;
+	char a;
+
+	for (i = 0; i > -1 ; i++)
+	{
+		a = s[i];
+		if (a == '\0')
+			break;
+	}
+	return (i);
 }
