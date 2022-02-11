c6f1ac59490db548d2185f4cbb023bf445dba138
Nachop51 <nachoperalta0@gmail.com>
2022-02-11 11:50:02 +0000

    Strrev test

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - prints a reverse string
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	printf("%s\n", strrev(s));
+}
