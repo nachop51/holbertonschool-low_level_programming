5daa7777a1f1e6ffbb3707b3853ed5d84335d80e
Nachop51 <nachoperalta0@gmail.com>
2022-02-11 17:20:31 +0000

    Strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcpy - copy a string
+ * @dest: dest
+ * @src: source
+ *
+ * Return: a char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
