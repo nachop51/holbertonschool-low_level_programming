518b4f0c3cf880dc23787000e0266457844ba2ac
Nachop51 <nachoperalta0@gmail.com>
2022-02-11 16:22:55 +0000

    Test

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - prints the half of a string
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int i, j = (strlen(str) - 1);
+
+	for (i = j / 2; i < j; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar(10);
+}
