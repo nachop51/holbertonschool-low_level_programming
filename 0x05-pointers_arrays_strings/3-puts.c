46b3c43be09632250deae3307f9f77d29b8f0136
Nachop51 <nachoperalta0@gmail.com>
2022-02-11 17:40:26 +0000

    Puts function

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -14,4 +13,24 @@ void _puts(char *str)
 		_putchar(str[i]);
 	}
 	_putchar('\n');
+}
+
+/**
+ * _strlen - string length
+ * @s: string
+ *
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	int i;
+	char a;
+
+	for (i = 0; i > -1 ; i++)
+	{
+		a = s[i];
+		if (a == '\0')
+			break;
+	}
+	return (i);
 }
