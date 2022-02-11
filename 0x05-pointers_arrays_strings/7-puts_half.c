64aee01cb39b189a14acb0fdeddb48dde8703171
Nachop51 <nachoperalta0@gmail.com>
2022-02-11 17:46:32 +0000

    Puts half function

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,11 +7,31 @@
  */
 void puts_half(char *str)
 {
-	int i, j = (strlen(str) - 1);
+	int i, j = (_strlen(str) - 1);
 
 	for (i = (j / 2) + 1; i <= j; i++)
 	{
 		_putchar(str[i]);
 	}
 	_putchar(10);
+}
+
+/**
+ * _strlen - string length
+ * @s: string
+ *
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	int i;
+	char a;
+
+	for (i = 0; i > -1 ; i++)
+	{
+		a = s[i];
+		if (a == '\0')
+			break;
+	}
+	return (i);
 }
