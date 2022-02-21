fdd5d33c07ffc62a6a3ea17cb0095e0cc01d78b0
Nachop51 <nachoperalta0@gmail.com>
2022-02-21 12:17:39 +0000

    strchr function, locates a char in a string and returns a pointer

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: char to locate
+ *
+ * Return: a char
+ */
+char *_strchr(char *s, char c)
+{
+	int i, found;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			found = 1;
+		}
+		if (found == 1)
+		{
+			s++;
+		}
+	}
+	if (found == 1)
+		return (s);
+	else
+		return (0);
+}
