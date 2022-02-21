74b973a8093b61481d1b43600ad9bccbd061edd0
Nachop51 <nachoperalta0@gmail.com>
2022-02-21 16:26:37 +0000

    Testing 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,7 +9,7 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i, found;
+	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
@@ -19,7 +19,5 @@ char *_strchr(char *s, char c)
 			break;
 		}
 	}
-	if (found == 1)
-		return (s);
-	return (0);
+	return (s);
 }
