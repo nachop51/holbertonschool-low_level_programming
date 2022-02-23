dbb4ede080519814422868d9488fff00e6b275a8
Nachop51 <nachoperalta0@gmail.com>
2022-02-23 17:15:24 +0000

    Testing square root recursive function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,9 +10,26 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n == 1)
-		return (1);
 
-	
-	return ();
+	return (calculateSquare(n, 1));
+}
+
+/**
+ * calculateSquare - calculates a square root
+ * @n: number
+ * @i: number
+ *
+ * Return: the square root
+ */
+int calculateSquare(int n, int i)
+{
+	if (i <= n)
+	{
+		if (i * i == n)
+			return (i);
+		else
+			return (calculateSquare(n, i + 1));
+	}
+	else
+		return (-1);
 }
