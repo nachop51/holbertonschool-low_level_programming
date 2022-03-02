16ac7f451c5de107e27ccd470c2e7dadb8c031a9
Nachop51 <nachoperalta0@gmail.com>
2022-03-02 14:18:56 +0000

    Fixing NULL cases

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,13 +12,17 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, n;
 	char *str;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	do {
 		i++;
 	} while (s1[i - 1]);
 	do {
 		j++;
 	} while (s2[j - 1]);
-	str = malloc(sizeof(char) * (i + j));
+	str = malloc(sizeof(char) * (i - 1 + j));
 	if (str == NULL)
 	{
 		return (NULL);
@@ -32,6 +36,6 @@ char *str_concat(char *s1, char *s2)
 	{
 		str[n + i - 1] = s2[n];
 	}
-	
+
 	return (str);
 }
