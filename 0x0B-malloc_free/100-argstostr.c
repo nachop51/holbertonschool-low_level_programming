c70e5468aa3faef987f4b11e6560b429ca2faa87
Nachop51 <nachoperalta0@gmail.com>
2022-03-02 18:26:15 +0000

    test

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+
+/**
+ * main - concatenates all arguments
+ * @ac: argument counter
+ * @av: argument vector
+ *
+ * Return: concatenated array
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j = 0, n = 0, count = 0;
+	char *s;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			j++;
+			count++;
+		}
+		count++;
+	}
+	count++;
+	s = malloc(sizeof(char) * count);
+	if (s == NULL)
+		return (NULL);
+
+	for (n = 0; n < ac; n++)
+	{
+		j = 0;
+		while(av[n][j])
+		{
+			s[count] = av[n][j];
+			j++;
+			count++;
+		}
+		s[count] = '\n';
+		count++;
+	}
+	s[count] = '\0';
+
+	return (s);
+}
