e2b4482937c52cee5a8cd9198ccde49c0fdb699e
Nachop51 <nachoperalta0@gmail.com>
2022-03-02 12:26:53 +0000

    Using NULL

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,9 +13,11 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *array;
 
-	array = malloc(sizeof(char *) * size);
 	if (size <= 0)
-		return (0);
+		return (NULL);
+	array = malloc(sizeof(char *) * size);
+	if (array == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
