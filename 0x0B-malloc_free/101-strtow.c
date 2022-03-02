6399257cf650dce98b291057d81c01db5b7f17a5
Nachop51 <nachoperalta0@gmail.com>
2022-03-02 20:15:59 +0000

    test

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -16,7 +16,7 @@ char **strtow(char *str)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0')
+		if (str[i] == ' ')
 		{
 			n++;
 		}
