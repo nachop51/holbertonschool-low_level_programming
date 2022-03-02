6bbe406a591b0827cee4c5014b4aa6f2306619cf
Nachop51 <nachoperalta0@gmail.com>
2022-03-02 12:59:45 +0000

    _strdup attempt

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+/**
+ * _strdup - strdup C function
+ * @str: string to duplicate
+ *
+ * Return: str
+ */
+char *_strdup(char *str)
+{
+	int i, n = 0;
+	char *strcopy;
+
+	if (str == NULL)
+		return (NULL);
+	do {
+		n++;
+	} while (str[n] != '\0');
+	strcopy = malloc(sizeof(char) * n);
+	if (strcopy == NULL)
+		return (NULL);
+
+	for (i = 0; i < n; i++)
+	{
+		strcopy[i] = str[i];
+	}
+
+	return (strcopy);
+}
