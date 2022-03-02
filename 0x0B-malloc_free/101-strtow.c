eb5532b7431790f7d30f8f9bf2da936dfee7a283
Nachop51 <nachoperalta0@gmail.com>
2022-03-02 19:49:54 +0000

    Testing split function

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * strtow - splits a string into words
+ * @str: string
+ *
+ * Return: an array of strings
+ */
+char **strtow(char *str)
+{
+	int i = 0, j = 0, n = 0, x = 0, a;
+	char **s;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	for (i = 0; str; i++)
+	{
+		if (str[i] == ' ')
+			n++;
+	}
+	printf("%d", n);
+	s = malloc(sizeof(char *) * n);
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] != ' ')
+			j++;
+		if (str[i] == ' ')
+		{
+			s[x] = malloc(sizeof(char) * j);
+			for (a = 0; a <= j; a++)
+			{
+				s[x][a] = str[a];
+			}
+			x++;
+			j = 0;
+			continue;
+		}
+	}
+	return (s);
+}
