d27407c8d8aff796420588b251fd69429323bc92
Nachop51 <nachoperalta0@gmail.com>
2022-03-02 12:40:41 +0000

    Testing malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,9 +13,9 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *array;
 
-	if (size <= 0)
+	if (size == 0)
 		return (NULL);
-	array = malloc(sizeof(char *) * size);
+	array = malloc(sizeof(char) * size);
 	if (array == NULL)
 		return (NULL);
 
