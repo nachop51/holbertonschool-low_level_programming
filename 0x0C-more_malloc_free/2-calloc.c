563341439364ac6a748170c850d281f4c407d69f
Nachop51 <nachoperalta0@gmail.com>
2022-03-03 16:34:24 +0000

    test

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *p;
 	unsigned int i = 0;
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 	p = malloc(nmemb * size);
 	if (p == NULL)
