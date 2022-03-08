cd358904b9bec2eb6f3d7f15dc73bed25f0dbed1
Nachop51 <nachoperalta0@gmail.com>
2022-03-08 15:37:49 +0000

    attempt to fix 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,7 +9,14 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
+	if (d->name == NULL)
+		return;
 	printf("Name: %s\n", d->name);
+	if (!d->age)
+		return;
 	printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+		return;
 	printf("Owner: %s\n", d->owner);
+
 }
