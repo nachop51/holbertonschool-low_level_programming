28f1eeef8641786d3524408575772a63fd2ada89
Nachop51 <nachoperalta0@gmail.com>
2022-03-08 16:20:16 +0000

    test

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -24,8 +24,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	d->name = malloc(sizeof(name));
 	d->name = strdup(name);
-	if (age < 0)
+	if (age > 0)
+	{
 		d->age = age;
+	}
 	else
 		i++;
 	if (owner == NULL)
