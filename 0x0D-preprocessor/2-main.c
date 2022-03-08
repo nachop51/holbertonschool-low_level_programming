7c488b5748ec49fd7728383000ef51ac362d509f
Nachop51 <nachoperalta0@gmail.com>
2022-03-08 11:28:50 +0000

    prints out the name of the program

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints out the name of the file
+ *
+ * Return: no error
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
