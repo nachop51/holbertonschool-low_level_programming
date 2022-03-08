967d145532bff5274e47d9d81afe678fc3c7c815
Nachop51 <nachoperalta0@gmail.com>
2022-03-08 17:12:38 +0000

    test

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,34 @@
 #include <string.h>
 
 /**
+ * _strdup - strdup C function
+ * @str: string to duplicate
+ *
+ * Return: str
+ */
+char *_strdup(char *str)
+{
+	int i, n = 0;
+	char *strcopy;
+
+	if (str == NULL)
+		return (NULL);
+	do {
+		n++;
+	} while (str[n - 1]);
+	strcopy = malloc(sizeof(char) * n);
+	if (strcopy == NULL)
+		return (NULL);
+
+	for (i = 0; i < n; i++)
+	{
+		strcopy[i] = str[i];
+	}
+
+	return (strcopy);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
@@ -17,21 +45,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
-	d->name = strdup(name);
+	d->name = _strdup(name);
 	if (!d->name)
 	{
 		free(d);
 		return (NULL);
 	}
 	d->age = age;
-	d->owner = strdup(owner);
+	d->owner = _strdup(owner);
 	if (!d->owner)
 	{
 		free(d->name);
 		free(d);
 		return (NULL);
 	}
-
 
 	return (d);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,7 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+char *_strdup(char *str);
 void free_dog(dog_t *d);
 
 #endif
