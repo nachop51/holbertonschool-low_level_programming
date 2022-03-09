c4dd481341150eb0bccb647949f3a5396110c87d
Nachop51 <nachoperalta0@gmail.com>
2022-03-09 15:57:58 +0000

    Program that performs simple operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - function that gives the function to use
+ * @s: operator
+ * Return: a pointer to the function
+ */
+int (*get_op_func(char *s))(int a, int b)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (ops[i].op[0] == s[0])
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - get the module of two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the module
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
