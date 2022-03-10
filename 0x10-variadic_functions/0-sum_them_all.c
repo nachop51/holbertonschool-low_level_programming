aee604b7b25b5a8c64636c215ec8892cc2f5160a
Nachop51 <nachoperalta0@gmail.com>
2022-03-10 12:32:03 +0000

    Sum all numbers

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum all passed numbers
+ * @n: number of arguments
+ * @...: n parameters to sum
+ *
+ * Return: the sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, asd = 0;
+	va_list args;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		asd += va_arg(args, int);
+	}
+	va_end(args);
+	return (asd);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef VARIADIC_H
+#define VARIADIC_H
+
+#include <stdarg.h>
+int sum_them_all(const unsigned int n, ...);
+
+#endif
