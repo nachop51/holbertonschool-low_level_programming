81283265a79014d671ad1a497ee4a9ed10a6ec3f
Nachop51 <nachoperalta0@gmail.com>
2022-03-10 15:23:29 +0000

    fixing previous fix

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,7 @@
 #ifndef VARIADIC_H
 #define VARIADIC_H
 
+#include <stdarg.h>
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
