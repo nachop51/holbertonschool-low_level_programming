9af3cf8dfbaf43fce7eb2ec5cb63c0608a06c632
Nachop51 <nachoperalta0@gmail.com>
2022-03-10 18:33:51 +0000

    juan 2

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -22,7 +22,7 @@ void print_all(const char * const format, ...)
 
 	va_start(args, format);
 
-	while (format[i] != '\0')
+	while (format[i] != '\0' && format != NULL)
 	{
 		j = 0;
 		while (form_ops[j].form != NULL)
