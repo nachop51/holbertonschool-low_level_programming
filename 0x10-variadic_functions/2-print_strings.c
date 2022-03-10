d55659573f70248ceab3db0a3b526216591734d5
Nachop51 <nachoperalta0@gmail.com>
2022-03-10 14:07:24 +0000

    Attempt to fix NULL string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,6 +9,7 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i, j = 0;
+	char *str;
 	va_list args;
 
 	if (n == 0)
@@ -18,12 +19,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
+		str = va_arg(args, char *);
 		if (i != 0 && j == 0)
 			printf("%s", separator);
-		if (va_arg(args, char *) == NULL)
-			printf("(nil)");
+		if (str != NULL)
+			printf("%s", str);
 		else
-			printf("%s", va_arg(args, char *));
+			printf("(nil)");
 	}
 	printf("\n");
 }
