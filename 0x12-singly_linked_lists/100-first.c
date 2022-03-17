a1c22253b21bea56b3977e65434a8b43b0f8025e
Nachop51 <nachoperalta0@gmail.com>
2022-03-17 18:35:46 +0000

    Function that executes before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print - prints a text before main executes
+ */
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -24,6 +24,7 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int _putchar(char);
+void print(void) __attribute__ ((constructor));
 char *_strdup(const char *str);
 int _strlen(const char *s);
 
