e5c9a71fe61f800abf482626759f514e14412136
Nachop51 <nachoperalta0@gmail.com>
2022-03-22 15:01:27 +0000

    fixing condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		(*head) = new;
 		return (aux);
 	}
-	for (i = 1; i < idx && aux->next; i++)
+	for (i = 1; i < idx && aux; i++)
 	{
 		aux = aux->next;
 		if (aux == NULL)
