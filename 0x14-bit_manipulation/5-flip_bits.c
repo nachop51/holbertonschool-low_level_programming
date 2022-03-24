6fb3fd32b200f35d51983aae4bfc62c1e8d10aa6
Nachop51 <nachoperalta0@gmail.com>
2022-03-24 21:43:45 +0000

    flips bits function

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - function that calculates how bits it'll take to get the other n
+ * @n: number
+ * @m: the other number
+ *
+ * Return:
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int i = 0, aux;
+
+	aux = n ^ m;
+	while (aux)
+	{
+		if ((aux & 1) == 1)
+			i++;
+		aux >>= 1;
+	}
+	return (i);
+}
