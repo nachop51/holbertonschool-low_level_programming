3264267dd95e1825037af52701fbf929f1958c60
Nachop51 <nachoperalta0@gmail.com>
2022-03-24 16:28:44 +0000

    print binary numbers

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,23 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-
-/**
- * power - elevates a to the power of b
- * @a: first parameter
- * @b: second parameter
- *
- * Return: an integer
- */
-unsigned int power(int a, int b)
-{
-	unsigned int result = 1;
-
-	for (; b > 0; b--)
-	{
-		result = result * a;
-	}
-	return (result);
-}
 
 /**
  * print_binary - prints out a binary number
@@ -25,17 +6,12 @@ unsigned int power(int a, int b)
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int j = 0, i = 0;
-
-	while (n)
+	if (n > 1)
 	{
-		while (n > j)
-			j = power(2, i), i++;
-		j = power(2, i - 1);
-		if ((n & j) == 1)
-			_putchar('1');
-		else
-			_putchar('0');
-		n = n >> 1;
+		print_binary(n >> 1);
 	}
+	if ((n & 1) == 1)
+		_putchar(49);
+	else
+		_putchar(48);
 }
