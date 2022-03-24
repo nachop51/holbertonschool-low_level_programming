48779afe60eb6c58b323dbaf3131f626d6d90d81
Nachop51 <nachoperalta0@gmail.com>
2022-03-24 16:43:49 +0000

    get bit from index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * get_bit - prints out the bit asked
+ * @n: number
+ * @index: index asked
+ *
+ * Return: the bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (((n >> index) & 1) == 1 || ((n >> index) & 1) == 0)
+		return ((n >> index) & 1);
+	else
+		return (-1);
+}
