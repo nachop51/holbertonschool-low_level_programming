56f7fb712ed16198530cc0b32f1b5432348bf551
Nachop51 <nachoperalta0@gmail.com>
2022-03-28 16:38:53 +0000

    Missing check

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-
+	if (!text_content)
+		text_content = "";
 	fd = open(filename, O_TRUNC | O_WRONLY);
 	if (fd == -1)
 	{
