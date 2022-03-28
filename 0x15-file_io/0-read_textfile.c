33f9768f234ac25d05cc792b2246e8f58355c227
Nachop51 <nachoperalta0@gmail.com>
2022-03-28 16:05:49 +0000

    Task 0, read text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * read_textfile - reads a text file and prints it
+ * @filename: name of the file
+ * @letters: number of letters
+ *
+ * Return: number of letters that it could read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd, b;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	b = read(fd, buf, letters);
+	if (b == -1)
+		return (0);
+
+	b = write(STDOUT_FILENO, buf, b);
+	if (b == -1)
+		return (0);
+
+	close(fd);
+
+	return (b);
+}
