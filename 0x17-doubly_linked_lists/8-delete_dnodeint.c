e028e5cd46efa64751bca32af6ab225f357ece18
Nachop51 <nachoperalta0@gmail.com>
2022-05-05 19:36:26 +0000

    Test

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -16,7 +16,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	node = *head;
 	if (index == 0)
 	{
-		if (node->next != NULL)
+		if ((*head)->next != NULL)
 		{
 			*head = (*head)->next;
 			(*head)->prev = NULL;
