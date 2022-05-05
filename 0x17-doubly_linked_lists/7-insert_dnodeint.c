651e1c2b7bca1297644d302a6c6c2887d54ffc9d
Nachop51 <nachoperalta0@gmail.com>
2022-05-05 18:42:24 +0000

    Fixing task 7, betty error

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->prev = node;
 	if (node->next == NULL)
 	{
-		free (new);
+		free(new);
 		return (add_dnodeint_end(h, n));
 	}
 	else
