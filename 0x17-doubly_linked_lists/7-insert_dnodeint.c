8245bbc5f1c6ee81645ab54e6aa75d12cd211730
Nachop51 <nachoperalta0@gmail.com>
2022-05-05 18:36:02 +0000

    Fixing task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!new)
 		return (NULL);
 	new->n = n;
-	while (i != idx - 1 && node != NULL)
+	while (i < idx - 1 && node != NULL)
 	{
 		node = node->next;
 		i++;
