c83376273e2ea2e35c5691e3a43401c21a2a06cd
Nachop51 <nachoperalta0@gmail.com>
2022-05-09 23:08:19 +0000

    test

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Nachop51/holbertonschool-low_level_programming/tree/main/0x18-dynamic_libraries/libfix.so
+wget -P /tmp https://raw.githubusercontent.com/Nachop51/holbertonschool-low_level_programming/main/0x18-dynamic_libraries/101-make_me_win.sh
 export LD_PRELOAD=/tmp/libfix.so
