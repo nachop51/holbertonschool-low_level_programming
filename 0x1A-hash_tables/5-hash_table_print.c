3caa14c3d007a7aa88619c6f56c64e993e22c82f
Nachop51 <nachoperalta0@gmail.com>
2022-05-27 18:01:11 +0000

    Fixing the previous version

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,40 +8,30 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	size_t ht_size = 0, flag = 0;
-	hash_node_t *aux = NULL, *exists = NULL;
+	hash_node_t *aux = NULL;
 
 	if (!ht)
 		return;
 	putchar(123);
+
 	while (ht_size < ht->size)
 	{
-		if (ht->array[ht_size])
-			exists = ht->array[ht_size];
-		ht_size++;
-	}
-
-	if (exists)
-	{
-		ht_size = 0;
-		while (ht_size < ht->size)
+		aux = ht->array[ht_size];
+		if (aux && aux->key)
 		{
-			aux = ht->array[ht_size];
-			if (aux && aux->key)
+			while (aux)
 			{
 				if (flag == 1)
 					printf(", ");
-				while (aux)
-				{
-					printf("'%s': '%s'", aux->key, aux->value);
-					if (!aux->next)
-						break;
-					aux = aux->next;
-				}
-				if (flag == 0)
-					flag++;
+				printf("'%s': '%s'", aux->key, aux->value);
+				if (!aux->next)
+					break;
+				aux = aux->next;
 			}
-			ht_size++;
+			if (flag == 0)
+				flag++;
 		}
+		ht_size++;
 	}
 	putchar(125);
 	putchar(10);
