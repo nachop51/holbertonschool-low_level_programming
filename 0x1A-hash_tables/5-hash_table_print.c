2f11476049a3ff39b0c0027fa271b39600da52a2
Nachop51 <nachoperalta0@gmail.com>
2022-05-27 17:46:38 +0000

    Trying to fix task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,28 +7,41 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	size_t ht_size = 0;
-	hash_node_t *aux = NULL;
+	size_t ht_size = 0, flag = 0;
+	hash_node_t *aux = NULL, *exists = NULL;
 
 	if (!ht)
 		return;
 	putchar(123);
-
 	while (ht_size < ht->size)
 	{
-		aux = ht->array[ht_size];
-		if (aux && aux->key)
-		{
-			while (aux)
-			{
-				printf("'%s': '%s'", aux->key, aux->value);
-				if (!aux->next)
-					break;
-				printf(", ");
-				aux = aux->next;
-			}
-		}
+		if (ht->array[ht_size])
+			exists = ht->array[ht_size];
 		ht_size++;
+	}
+
+	if (exists)
+	{
+		ht_size = 0;
+		while (ht_size < ht->size)
+		{
+			aux = ht->array[ht_size];
+			if (aux && aux->key)
+			{
+				if (flag == 1)
+					printf(", ");
+				while (aux)
+				{
+					printf("'%s': '%s'", aux->key, aux->value);
+					if (!aux->next)
+						break;
+					aux = aux->next;
+				}
+				if (flag == 0)
+					flag++;
+			}
+			ht_size++;
+		}
 	}
 	putchar(125);
 	putchar(10);
