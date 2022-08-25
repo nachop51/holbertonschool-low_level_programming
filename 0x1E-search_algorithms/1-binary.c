7c2cfb514cc38957464ebf7437f491a9a2445e84
Nachop51 <nachoperalta0@gmail.com>
2022-08-25 15:51:32 +0000

    Finishing task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,29 +1,34 @@
 #include "search_algos.h"
 
+/**
+ * binary_helper - Searchs a value in an array using binary searching
+ * @array: Array to search
+ * @value: Value to search
+ * @low: Bottom top
+ * @high: Up top
+ *
+ * Return: -1 if fails, else the index of the value in the array
+ */
 int binary_helper(int *array, int value, int low, int high)
 {
-	int mid = low + (high - low) / 2, i;
+		int mid = low + (high - low) / 2, i = 0;
 
-	printf("Searching in array: ");
-	for (i = low; i <= high; i++)
-	{
-		if (i != low)
-			printf(", ");
-		printf("%d", array[i]);
-	}
-	printf("\n");
-
-	if (high >= low) {
-		mid = low + (high - low) / 2;
-
-		if (array[mid] == value)
-			return mid;
-		if (array[mid] > value)
-			return (binary_helper(array, value, low, mid - 1));
-		return (binary_helper(array, value, mid + 1, high));
-	}
-	return (-1);
-}
+		if (high >= low) {
+				printf("Searching in array: ");
+		    for (i = low; i <= high; i++)
+		    {
+		        if (i != low)
+		            printf(", ");
+		        printf("%d", array[i]);
+		    }
+		    putchar(10);
+		    if (array[mid] == value)
+		        return mid;
+		    if (array[mid] > value)
+		        return binary_helper(array, value, low, mid - 1);
+		    return binary_helper(array, value, mid + 1, high);
+		}
+		return (-1);
 
 /**
  * binary_search - Searchs a value in an array using binary searching
@@ -35,7 +40,7 @@ int binary_helper(int *array, int value, int low, int high)
  */
 int binary_search(int *array, size_t size, int value)
 {
-	if (!array)
-		return (-1);
-	return (binary_helper(array, value, 0, size - 1));
+		if (!array)
+				return (-1);
+		return (binary_helper(array, value, 0, size - 1));
 }
