8c1b3b2c58d21f7d4c0dfeb3b4a9d59aa1b4fea9
Nachop51 <nachoperalta0@gmail.com>
2022-08-25 15:58:47 +0000

    fixing task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,15 +12,15 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if (!array)
+	if (!array || size == 0)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
 			return (i);
 		}
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 	}
 	return (-1);
 }
